7d1a76c129f8b028d2096277d0c5b4eeb4fd2e34
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-08 20:13:23 -0500

    Cleaning up, adding constructor

--- connect4.js
+++ connect4.js
@@ -1,4 +1,13 @@
 class Game {
+  constructor(p1, p2, height = 6, width = 7) {
+    this.players = [p1, p2];
+    this.height = height;
+    this.width = width;
+    this.currPlayer = p1;
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.gameOver = false;
+  }
   makeBoard() {
     const { width, height } = this;
     for (let y = 0; y < height; y++) {
@@ -8,12 +17,16 @@ class Game {
 
   makeHtmlBoard() {
     const board = document.getElementById("board");
+    board.innerHTML = "";
 
     const top = document.createElement("tr");
     top.setAttribute("id", "column-top");
-    top.addEventListener("click", handleClick);
 
-    for (let x = 0; x < WIDTH; x++) {
+    this.handleGameClick = this.handleClick.bind(this);
+
+    top.addEventListener("click", this.handleGameClick);
+
+    for (let x = 0; x < this.width; x++) {
       const headCell = document.createElement("td");
       headCell.setAttribute("id", x);
       top.append(headCell);
